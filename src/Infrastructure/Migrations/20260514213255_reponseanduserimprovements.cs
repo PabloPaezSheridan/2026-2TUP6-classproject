9ca84734eedaf3f955570b15083ecb440d4efb9a
Pablo Paez <pablopaezsheridan@outlook.com>
2026-05-14 18:43:16 -0300

    CLass 7: add migration to update user with new attributes
    tu cachorrita

---
+++ src/Infrastructure/Migrations/20260514213255_reponseanduserimprovements.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class reponseanduserimprovements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "IsActive", "Role" },
+                values: new object[] { true, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "IsActive", "Role" },
+                values: new object[] { true, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+    }
+}
---
+++ src/web/DTOs/CredentialsRequest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace web.DTOs;
+
+public class CredentialsRequest
+{
+    public string Password {get;set;}
+    public string Username {get;set;}
+
+}
